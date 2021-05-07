5e39ea265463247824aec066dd9d8e2a7459ba88
Him <devophx@gmail.com>
2021-05-08 01:16:28 +0300

    Fun with Permutations using iterationtool

---
+++ perm.py
@@ -0,0 +1,9 @@
+import string
+import itertools
+
+file1 = open("perm_wordlist.txt","w");
+for x in itertools.product('abcdefghijklmnopqrst', repeat=10):
+    y= ''.join(x)
+    file1.write(y)
+    file1.write("\n")
+file1.close()
